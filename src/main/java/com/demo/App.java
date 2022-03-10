5d1f91378ce758bc0c866f54dcf4469894e5619b
Shubham Shinde <shushind@publicisgroupe.net>
2022-03-10 11:12:17 +0530

    project uploaded with new

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Greeter g = new Greeter();
+    	g.greetMessage();
+    	
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/com/demo/Greeter.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class Greeter {
+
+	public void greetMessage() {
+		System.out.println("Hello Good Morning");
+	}
+}
